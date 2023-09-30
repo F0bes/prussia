bf05fca1021e4149d3fdb813387c53aa67538662
Ty Lamontagne <amfobes@gmail.com>
2023-09-29 20:48:50 -0400

    prussia_bios: Add some more syscalls

--- prussia_bios/src/lib.rs
+++ prussia_bios/src/lib.rs
@@ -39,9 +39,9 @@ pub fn reset_ee(reset_flag: u32) {
 #[repr(i16)]
 pub enum VideoMode {
     /// PAL, 720x576@50Hz interlaced, as used in most of Europe.
-    Pal = 2,
+    NTSC = 2,
     /// NTSC, 720x480@59.97Hz interlaced, as used in North America and Japan.
-    Ntsc = 3,
+    PAL = 3,
 }
 
 /// Whether interlacing is enabled, passed to `set_gs_crt`.
@@ -124,45 +124,44 @@ pub fn exec_ps2(entry: u32, gp: u32, argc: i32, argv: *const *const u8) {
     }
 }
 
-    /// The interrupt controller status. Each flag is set if the interrupt controller detects a
-    /// change in state. Writing a bit acknowledges that interrupt. If the status and
-    /// corresponding mask bit are both set, INT0 is raised.
-    pub enum INTCCause {
-        /// An interrupt from the Graphics Synthesizer.
-        GS,
-        /// An interrupt from the SBUS connection to the IOP.
-        SBUS,
-        /// The start of VBlank.
-        VBON,
-        /// The end of VBlank.
-        VBOF,
-        /// An interrupt from VU Interface 0.
-        VIF0,
-        /// An interrupt from VU Interface 1.
-        VIF1,
-        /// An interrupt from Vector Unit 0.
-        VU0,
-        /// An interrupt from Vector Unit 1.
-        VU1,
-        /// An interrupt from the Image Processing Unit.
-        IPU,
-        /// An interrupt from Timer 0.
-        TIM0,
-        /// An interrupt from Timer 1.
-        TIM1,
-        /// An interrupt from Timer 2.
-        TIM2,
-        /// An interrupt from Timer 3.
-        TIM3,
-        /// An interrupt from the SBUS FIFO queue.
-        SFIFO,
-        /// An interrupt from the Vector Unit 0 watchdog.
-        VU0WD,
-    }
+/// The interrupt controller status. Each flag is set if the interrupt controller detects a
+/// change in state. Writing a bit acknowledges that interrupt. If the status and
+/// corresponding mask bit are both set, INT0 is raised.
+pub enum INTCCause {
+    /// An interrupt from the Graphics Synthesizer.
+    GS,
+    /// An interrupt from the SBUS connection to the IOP.
+    SBUS,
+    /// The start of VBlank.
+    VBON,
+    /// The end of VBlank.
+    VBOF,
+    /// An interrupt from VU Interface 0.
+    VIF0,
+    /// An interrupt from VU Interface 1.
+    VIF1,
+    /// An interrupt from Vector Unit 0.
+    VU0,
+    /// An interrupt from Vector Unit 1.
+    VU1,
+    /// An interrupt from the Image Processing Unit.
+    IPU,
+    /// An interrupt from Timer 0.
+    TIM0,
+    /// An interrupt from Timer 1.
+    TIM1,
+    /// An interrupt from Timer 2.
+    TIM2,
+    /// An interrupt from Timer 3.
+    TIM3,
+    /// An interrupt from the SBUS FIFO queue.
+    SFIFO,
+    /// An interrupt from the Vector Unit 0 watchdog.
+    VU0WD,
+}
 
 /// Add an interrupt handler
-/// next = 0 -> add to front of chain
-/// next = -1 -> add to end of chain
+/// Returns the handler ID if the operation succeeds, -1 if it fails
 pub fn add_intc_handler(int_cause: INTCCause, handler: fn(i32) -> i32, next: i32) -> i32 {
     let result: i32;
     unsafe {
@@ -170,9 +169,84 @@ pub fn add_intc_handler(int_cause: INTCCause, handler: fn(i32) -> i32, next: i32
             "syscall",
             out("$2") result, // v0
             in("$3") 0x10, // v1
-            in("$4") int_cause as i32,
-            in("$5") handler as usize,
-            in("$6") next,
+            in("$4") int_cause as i32, // a0
+            in("$5") handler as usize, // a1
+            in("$6") next, // a2
+        );
+    }
+    result
+}
+
+/// Remove an interrupt handler
+/// Returns -1 if the operation fails
+pub fn remove_intc_handler(int_case: INTCCause, handler_id: i32) -> i32 {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x11, // v1
+            in("$4") int_case as i32, // a0
+            in("$5") handler_id, // a1
+        );
+    }
+    result
+}
+
+/// The interrupt controller status. Each flag is set if the interrupt controller detects a
+/// change in state. Writing a bit acknowledges that interrupt. If the status and
+/// corresponding mask bit are both set, INT0 is raised.
+pub enum DMACause {
+    /// An interrupt from the Vector InterFace 0 channel.
+    VIFO,
+    /// An interrupt from the Vector InterFace 1 channel.
+    VIF1,
+    /// An interrupt from the GIF channel.
+    GIF,
+    /// An interrupt from the IPU FROM channel.
+    FROMIPU,
+    /// An interrupt from the IPU TO channel.
+    TOIPU,
+    /// An interrupt from the Serial InterFace 0 channel.
+    SIF0,
+    /// An interrupt from the Serial InterFace 1 channel.
+    SIF1,
+    /// An interrupt from the Serial InterFace 2 channel.
+    SIF2,
+    /// An interrupt from the Scratch Pad Ram FROM channel.
+    FROMSPR,
+    /// An interrupt from the Scratch Pad Ram TO channel.
+    TOSPR
+}
+
+/// Add a DMAC handler
+/// Returns the handler ID if the operation succeeds, -1 if it fails
+pub fn add_dmac_handler(dma_cause: DMACause, handler: fn(i32) -> i32, next: i32) -> i32 {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x12, // v1
+            in("$4") dma_cause as i32, // a0
+            in("$5") handler as usize, // a1
+            in("$6") next, // a2
+        );
+    }
+    result
+}
+
+/// Remove a DMAC handler
+/// Returns -1 if the operation fails
+pub fn remove_dmac_handler(dma_cause: DMACause, handler_id: i32) -> i32 {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x13, // v1
+            in("$4") dma_cause as i32, // a0
+            in("$5") handler_id, // a1
         );
     }
     result
@@ -187,7 +261,52 @@ pub fn enable_intc(int_cause: INTCCause) -> bool {
             "syscall",
             out("$2") result, // v0
             in("$3") 0x14, // v1
-            in("$4") int_cause as i32,
+            in("$4") int_cause as i32, // a0
+        );
+    }
+    result == 1
+}
+
+/// Disables the bit in the interrupt mask register.
+/// Returns true if the bit was set to 1 and false if it was already set to 0.
+pub fn disable_intc(int_cause: INTCCause) -> bool {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x15, // v1
+            in("$4") int_cause as i32, // a0
+        );
+    }
+    result == 1
+}
+
+/// Enables the bit in the dmac interrupt mask register.
+/// Returns true if the bit was set to 0 and false if it was already set to 1.
+pub fn enable_dmac(int_cause: DMACause) -> bool {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x16, // v1
+            in("$4") int_cause as i32, // a0
+        );
+    }
+    result == 1
+}
+
+/// Disables the bit in the dmac interrupt mask register.
+/// Returns true if the bit was set to 1 and false if it was already set to 0.
+pub fn disable_dmac(int_cause: DMACause) -> bool {
+    let result: i32;
+    unsafe {
+        asm!(
+            "syscall",
+            out("$2") result, // v0
+            in("$3") 0x17, // v1
+            in("$4") int_cause as i32, // a0
         );
     }
     result == 1
